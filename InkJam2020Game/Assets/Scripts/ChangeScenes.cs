2bc5f7621b05f706faea85e902ac4633f50b92de
Stav Hinenzon <stav.hinenzon@gmail.com>
2020-08-02 21:21:44 -0700

    Added main menu

---
+++ InkJam2020Game/Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+  public float FadeTime = 1.0f;
+  public Canvas firstCanvas;
+
+  string dest;
+  Canvas visibleCanvas;
+  Coroutine fadeRoutine;
+
+  private void Awake()
+  {
+    visibleCanvas = firstCanvas;
+  }
+
+  // Use this for initialization
+	void Start ()
+  {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+	}
+
+  public void SwapScene(string scene)
+  {
+    dest = scene;
+    if (fadeRoutine != null)
+    {
+      StopCoroutine(fadeRoutine);
+    }
+    fadeRoutine = StartCoroutine("SwapOut");
+  }
+
+  public void SwapCanvas(string canvasName)
+  {
+    foreach (var canvas in FindObjectsOfType<Canvas>())
+    {
+      if (canvas.gameObject.name.Equals(canvasName))
+      {
+        visibleCanvas.enabled = false;
+        visibleCanvas = canvas;
+        visibleCanvas.enabled = true;
+      }
+    }
+  }
+
+  public void QuitGame()
+  {
+    dest = "quit";
+    if (fadeRoutine != null)
+    {
+      StopCoroutine(fadeRoutine);
+    }
+    fadeRoutine = StartCoroutine("SwapOut");
+  }
+
+  IEnumerator SwapOut()
+  {
+    var canvasGroup = visibleCanvas.GetComponent<CanvasGroup>();
+    
+    float fadeRate = (1 / FadeTime) * Time.deltaTime;
+    for (float f = 1.0f; f > 0.0f; f -= fadeRate)
+    {
+      canvasGroup.alpha = f;
+      yield return null;
+    }
+    
+    if (dest == "quit")
+    {
+      Application.Quit();
+    }
+    else
+    {
+      SceneManager.LoadScene(dest);
+    }
+  }
+}
